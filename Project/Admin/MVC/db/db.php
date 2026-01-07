5c183181b0f263ec561b0eff9ff90d3985d4f291
SRsieam <srsieam2003@gmail.com>
2026-01-07 18:17:25 +0600

    add database configuration

---
+++ Project/Admin/MVC/db/db.php
@@ -0,0 +1,15 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "crimealert");
+
+if (!$conn) {
+    die("Database connection failed");
+}
+
+function test_input($data) {
+    global $conn;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return mysqli_real_escape_string($conn, $data);
+}
+?>
