55b807cb3e8496518e9d1f3e2cbe04f755c6b24a
asifahmed1467 <rockyasif825@gmail.com>
2026-01-15 14:32:32 +0600

    home html

---
+++ PROJECT/Management/User/MVC/html/home_view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Crime Alert</title>
+    <link rel="stylesheet" type="text/css" href="../css/home_style.css">
+</head>
+<body>
+
+    <div class="welcome-card">
+        <span class="icon">🛡️</span>
+        <h1>Crime Alert System</h1>
+        <p>Welcome! Please select an option to continue.</p>
+
+        <a href="../../../Admin/MVC/php/admin_login_controller.php" class="btn btn-admin">Admin Login</a>
+
+        <a href="login_controller.php" class="btn btn-user">User Login</a>
+
+        <a href="registration_controller.php" class="btn btn-new">New User Registration</a>
+    </div>
+
+</body>
+</html>
