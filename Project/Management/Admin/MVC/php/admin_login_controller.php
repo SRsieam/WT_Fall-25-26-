13cd78d844209816c2d69f0df54954244024a4df
SRsieam <srsieam2003@gmail.com>
2026-01-18 21:56:38 +0600

    add logic part of mvc, login controller

---
+++ Project/Management/Admin/MVC/php/admin_login_controller.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+include '../db/db.php';
+
+$error = "";
+
+if (isset($_POST['login_btn'])) {
+
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    $sql = "SELECT * FROM admin WHERE email='$email' AND password='$password'"; 
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['is_admin_logged_in'] = true;
+        $_SESSION['admin_email'] = $row['email'];
+
+        header("Location: admin_dashboard_controller.php");
+        exit();
+
+    } else {
+
+        $error = "Invalid Email or Password";
+
+    }
+}
+
+include '../html/admin_login_view.php';
+?>
