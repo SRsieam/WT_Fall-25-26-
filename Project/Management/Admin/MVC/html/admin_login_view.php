536e355351b96ee0b376c01b8ea4429bab670e0d
SRsieam <srsieam2003@gmail.com>
2026-01-18 21:41:20 +0600

    view part of MVC, add admin login part

---
+++ Project/Management/Admin/MVC/html/admin_login_view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="../css/admin_login.css">
+</head>
+<body>
+
+    <div class="login-card">
+        <h2>Admin Panel</h2>
+        <p>Please login to manage the system</p>
+
+        <?php if($error): ?>
+            <div class="error-msg"><?php echo $error; ?></div>
+        <?php endif; ?>
+
+        <form action="" method="POST">
+            <div class="form-group">
+                <label>Email Address</label>
+                <input type="email" name="email" placeholder="admin@test.com" required>
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter your password" required>
+            </div>
+
+            <button type="submit" name="login_btn">Login</button>
+        </form>
+    </div>
+
+</body>
+</html>
