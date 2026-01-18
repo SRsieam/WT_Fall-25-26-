c7d876d34c5449fbcd0213fd57622c2c9cff814f
SRsieam <srsieam2003@gmail.com>
2026-01-18 22:00:07 +0600

    reports page controller/logic operation

---
+++ Project/Management/Admin/MVC/php/admin_reports_controller.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include '../db/db.php'; 
+
+if (!isset($_SESSION['is_admin_logged_in'])) {
+    header("Location: admin_login_controller.php");
+    exit();
+}
+
+$msg = "";
+
+if (isset($_POST['approve_report'])) {
+    $id = intval($_POST['report_id']);
+    $sql = "UPDATE posts SET status='Resolved' WHERE id=$id";
+    if (mysqli_query($conn, $sql)) {
+        $msg = "<div class='alert success'>Report #$id marked as Resolved</div>";
+    }
+}
+
+if (isset($_POST['delete_report'])) {
+    $id = intval($_POST['report_id']);
+    $sql = "DELETE FROM posts WHERE id=$id";
+    if (mysqli_query($conn, $sql)) {
+        $msg = "<div class='alert danger'>Report #$id has been deleted</div>";
+    }
+}
+
+$sql = "SELECT p.*, u.name as reporter_name, u.email as reporter_email, c.category_name 
+        FROM posts p 
+        JOIN users u ON p.user_id = u.id 
+        LEFT JOIN categories c ON p.category_id = c.id
+        ORDER BY p.created_at DESC";
+$result = mysqli_query($conn, $sql);
+
+include '../html/admin_reports_view.php';
+?>
